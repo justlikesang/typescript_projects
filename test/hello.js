4a9cb7c64d73b0f3d1de08ec859d38a4cdc0bb2f
Sang <sanghosuh@gmail.com>
2022-03-11 20:13:33 -0800

    todo-practice

---
+++ test/hello.js
@@ -0,0 +1,13 @@
+// // Greets the world.
+// console.log("Hello world!");
+// // This is an industrial-grade general-purpose greeter function:
+// function greet(person: string, date: Date) {
+//   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+// greet("Brendan", new Date());
+var msg = "hello there!";
+// "use strict";
+// function greet(person, date) {
+//     console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+// }
+// greet("Maddison", new Date());
---
+++ test/hello.ts
@@ -0,0 +1,18 @@
+// // Greets the world.
+// console.log("Hello world!");
+
+
+// // This is an industrial-grade general-purpose greeter function:
+// function greet(person: string, date: Date) {
+//   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+ 
+// greet("Brendan", new Date());
+
+let msg = "hello there!";
+
+// "use strict";
+// function greet(person, date) {
+//     console.log("Hello " + person + ", today is " + date.toDateString() + "!");
+// }
+// greet("Maddison", new Date());
